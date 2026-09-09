79d6ed8bc30f5a194e3d77689ae07244c66bf87c
Nguyen Chi Bao <ncbslav@gmail.com>
2026-09-09 10:39:01 +0700

    input validation for number of students

--- main.cpp
+++ main.cpp
@@ -1,1 +1,21 @@
 #include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+
+    do {
+        cout << "Nhap so luong hoc sinh N (2 <= N <= 20): ";
+        cin >> n;
+
+        // Kiem tra dieu kien n hop le
+        if (n < 2 || n > 20) {
+            cout << "So luong khong hop le! Vui long nhap lai so luong tu 2 den 20.\n\n";
+        }
+    } while (n < 2 || n > 20);
+
+    cout << "So luong hoc sinh hop le da nhap: " << n << endl;
+
+    return 0;
+}
